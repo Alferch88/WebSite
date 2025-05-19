f0357b7f7e6a1d84ba648ac2a4ed0f2c51fa6ee0
alferch88 <shatilo_vlad@inbox.ru>
2025-05-19 19:47:12 +0600

    Добавлен код

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-# ParsingWebSite - стартовый файл
+import Parser
+url = 'https://omsk.hh.ru/search/vacancy?text=python&area=68'
+if __name__ == '__main__':
+    try:
+        Parser.parse(url)
+    except:
+        print("Ошибка")
+     
 
-print("Добро пожаловать в проект ParsingWebSite!")
+
